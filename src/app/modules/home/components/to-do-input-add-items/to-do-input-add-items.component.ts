ecd01dba949868848271f514269ad295819187dd
Leandro Rangel <rangel94@outlook.com>
2023-07-18 11:40:21 -0300

    Configurações iniciais do projeto

---
+++ src/app/modules/home/components/to-do-input-add-items/to-do-input-add-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-input-add-items',
+  templateUrl: './to-do-input-add-items.component.html',
+  styleUrls: ['./to-do-input-add-items.component.scss']
+})
+export class ToDoInputAddItemsComponent {
+
+}
