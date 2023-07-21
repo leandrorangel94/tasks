68f75646a0ec4a276b6fcfd260ab19c3a76920be
Leandro Rangel <rangel94@outlook.com>
2023-07-21 18:06:45 -0300

    adicionado funções de deletar tarefas

--- src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -8,11 +8,20 @@ import { TaskList } from '../../model/task-list';
 })
 export class ToDoListComponent implements OnInit {
   public taskList: Array<TaskList> = [
-    // { task: 'Nova task', checked: true },
-    // { task: 'Nova task 2', checked: false },
+    { task: 'Nova task', checked: true },
+    { task: 'Nova task 2', checked: false },
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public deleteTaskListItem(event: number): void {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskListItems(): void {
+    const confirm = window.confirm('Você realmente deseja deletar tudo?');
+    if (confirm) this.taskList = [];
+  }
 }
